3bd2d6eb9a112ff93dc75bd5697d2e09ba2a74d0
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-02 14:41:49 +0300

    All tasks are completed.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int isLeapYear;
+        if( ((year% 4 == 0) && (year% 100 != 0)) || (year% 400 == 0) ){
+            isLeapYear = 1;
+        }else{
+            isLeapYear = 0;
+        }
+
+        switch (isLeapYear){
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+
+
+        }
     }
 }
